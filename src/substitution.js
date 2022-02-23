af65508d12410198fcc06653e8e17efb6b814520
dan <da.ga.mo.94@gmail.com>
2022-02-23 12:42:08 -0700

    add helper functions to code and decode in the substitution function. Cleans up substitution()

--- src/substitution.js
+++ src/substitution.js
@@ -6,6 +6,44 @@
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
+  function encodeMessage(message, abc, alphabet) {
+    let res = '';
+    for (const currChar of message) {
+      // get index of the current character according to the 'abc' array
+      let currCharIndex = abc.indexOf(currChar);
+
+      // Check if the current value is a letter, then code/decode
+      if (currChar.match(/[a-z]/)) {
+        res += alphabet[currCharIndex];
+
+        // Else just add the current character to the resulting string without any changes
+      } else {
+        res += currChar;
+      }
+    }
+    return res;
+  }
+
+  
+  function decodeMessage(message, abc, alphabet) {
+    let result = '';
+    for (const currChar of message) {
+      // get index of the current character according to the 'alphabet' string
+      let currCharIndex = alphabet.indexOf(currChar);
+
+      // Check if the current value is not a space
+      if (!currChar.match(/\s+/g)) {
+        result += abc[currCharIndex];
+
+        // Else just add the current character to the resulting string without any changes
+      } else {
+        result += currChar;
+      }
+    }
+    return result;
+  }
+
+
   function substitution(input, alphabet, encode = true) {
     // your solution code here
     const abc = 'abcdefghijklmnopqrstuvwxyz';
@@ -13,46 +51,20 @@ const substitutionModule = (function () {
     let result = '';
 
     // Check if an alphabet is being passed in or if it is exactly 26 characters
-    if(!alphabet || alphabet.length !== 26 || !input) return false;
-    
+    if (!alphabet || alphabet.length !== 26 || !input) return false;
+
     // Check if 'alphabet' has any repeated characters
     let anyDuplicates = new Set(alphabet)
     if ([...anyDuplicates].length !== 26) return false;
 
-    if(encode){
-      for(const currChar of message){
-        // get index of the current character according to the 'abc' array
-        let currCharIndex = abc.indexOf(currChar);
-  
-        // Check if the current value is a letter, then code/decode
-        if(currChar.match(/[a-z]/)){
-          result += alphabet[currCharIndex];
-        
-        // Else just add the current character to the resulting string without any changes
-        }else{
-          result += currChar;
-        }
-      }
-      // Return the 'result' string and stop the rest of the function's execution
+    // Check if we need to encode, if so execute encodeMessage(), else execute decodeMessage()
+    if (encode) {
+      result = encodeMessage(message, abc, alphabet);
       return result;
     }
-
-    for(const currChar of message){
-      // get index of the current character according to the 'alphabet' string
-      let currCharIndex = alphabet.indexOf(currChar);
-
-      // Check if the current value is not a space
-      if(!currChar.match(/\s+/g)){
-        result += abc[currCharIndex];
-      
-      // Else just add the current character to the resulting string without any changes
-      }else{
-        result += currChar;
-      }
-    }
-
-    return result;
-
+    
+    // Decode if encode is false
+    return result = decodeMessage(message, abc, alphabet);
   }
 
   return {
