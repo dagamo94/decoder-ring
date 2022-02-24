7bb9b5364f50a56d7b5e8b362d01a7593bdc8c78
dan <da.ga.mo.94@gmail.com>
2022-02-24 02:48:44 -0700

    refactored and cleaned up polybius.js

--- src/polybius.js
+++ src/polybius.js
@@ -10,56 +10,47 @@ const polybiusModule = (function () {
   const encoder = { a: '11', b: '21', c: '31', d: '41', e: '51', f: '12', g: '22', h: '32', i: '42', j: '42', k: '52', l: '13', m: '23', n: '33', o: '43', p: '53', q: '14', r: '24', s: '34', t: '44', u: '54', v: '15', w: '25', x: '35', y: '45', z: '55' };
   const decoder = { '11': 'a', '21': 'b', '31': 'c', '41': 'd', '51': 'e', '12': 'f', '22': 'g', '32': 'h', '42': '(i/j)', '52': 'k', '13': 'l', '23': 'm', '33': 'n', '43': 'o', '53': 'p', '14': 'q', '24': 'r', '34': 's', '44': 't', '54': 'u', '15': 'v', '25': 'w', '35': 'x', '45': 'y', '55': 'z' };
 
-  // function encoder(){
-
-  // }
-
-  function polybius(input, encode = true) {
-    //declare variable to hold encoded/decoded letter/num combination
+  function encodeMessage(message) {
     let result = '';
-    let message = input.toLowerCase();
+    let tempWordArr = message.split(' ');
+    let encoded = [];
 
-    if (!encode && input.replace(/\s+/g, '').length % 2 > 0) {
-      return false;
-    }
-
-    if (encode) {
-      let tempWordArr = message.split(' ');
-      let encoded = [];
-
-      //console.log("Temp Word Arr: " + tempWordArr);
-      for (const word of tempWordArr) {
-        for (let i = 0; i < word.length; i++) {
-          let character = word[i]
-          encoded.push(encoder[character]);
-        }
-
-        result += `${encoded.join("")} `;
-        encoded = [];
+    for (const word of tempWordArr) {
+      for (let i = 0; i < word.length; i++) {
+        let character = word[i]
+        encoded.push(encoder[character]);
       }
-      //console.log("Result: " + result);
-
-      return result.slice(0, -1);
+      result += `${encoded.join("")} `;
+      encoded = [];
     }
+    
+    return result.slice(0, -1);
+  }
 
-    // Decoder
+
+  function decodeMessage(message) {
+    let result = '';
     let tempWordArr = message.split(' ');
     let decoded = [];
 
-    //console.log("Temp Word Arr: " + tempWordArr);
     for (const word of tempWordArr) {
       for (let i = 0; i < word.length; i += 2) {
-        let number = `${word[i]}${word[i+1]}`
+        let number = `${word[i]}${word[i + 1]}`
         decoded.push(decoder[number]);
       }
-
       result += `${decoded.join("")} `;
       decoded = [];
     }
-    //console.log("Result: " + result);
 
     return result.slice(0, -1);
+  }
 
+  function polybius(input, encode = true) {
+    let message = input.toLowerCase();
+
+    if (!encode && input.replace(/\s+/g, '').length % 2 > 0) return false;
+    if (encode) return encodeMessage(message);
+    return decodeMessage(message);
   }
 
   return {
